64109b3ff64054cb30b41f987ac3b4ffa6c2fe78
Alexsander <alexsander11115@gmail.com>
2020-01-14 16:58:36 +0200

    Add post method

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -5,12 +5,25 @@ def home(request):
     import json
     import requests
 
-    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=49098&distance=5&API_KEY=7A895D99-D8D7-42D1-B90A-08694937172E")
+    if request.method == "POST":
+        zipcode = request.POST['zipcode']
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+        api_request = requests.get(
+            "http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode="+ zipcode +"&distance=5&API_KEY=7A895D99-D8D7-42D1-B90A-08694937172E")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+
+        return render(request, 'lookup/home.html', {'zipcode':zipcode,'api':api})
+    else:
+        api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=49098&distance=5&API_KEY=7A895D99-D8D7-42D1-B90A-08694937172E")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
 
     return render(request, 'lookup/home.html', {'api': api})
 

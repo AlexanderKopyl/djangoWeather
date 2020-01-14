b7ae045535898c28290b4f5979e4a5274d6bf6c1
Alexsander <alexsander11115@gmail.com>
2020-01-14 15:09:35 +0200

    Add api

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'lookup/home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=49098&distance=5&API_KEY=7A895D99-D8D7-42D1-B90A-08694937172E")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'lookup/home.html', {'api': api})
 
 
 def about(request):
